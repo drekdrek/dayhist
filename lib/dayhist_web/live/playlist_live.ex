e4a0b52ed735a315b94f48f57e58e54c708d8633
Owen Halliday <hallidayo@moravian.edu>
2024-08-10 12:41:21 -0400

    add playlist creation and description

--- lib/dayhist_web/live/playlist_live.ex
+++ lib/dayhist_web/live/playlist_live.ex
@@ -4,6 +4,7 @@ defmodule DayhistWeb.PlaylistLive do
 
   alias Dayhist.Schemas.{Daylist, Track}
   import Ecto.Query
+  # alias Dayhist.SpotifyAPI.AccessToken
 
   @uuid_regex ~r/([\d\w]{8})([\d\w]{4})([\d\w]{4})([\d\w]{4})([\d\w]{12})/
 
@@ -13,8 +14,39 @@ defmodule DayhistWeb.PlaylistLive do
       |> assign(:playlist, nil)
       |> assign(:playlist_owner, nil)
       |> assign(:user_info, session["spotify_info"])
+      |> assign(:created_playlist_id, nil)
 
     {:ok, socket, layout: {DayhistWeb.Layouts, :playlist}}
+  end
+
+  defp _maybe_get_spotify_playlist_url(user_id, _playlist) do
+    # if the user is not logged in, return empty string,
+    # if they do, query the SpotifyApi to see if they have a playlist matching with this playlist. if they do, return that url, otherwise return empty string
+    if user_id do
+      "example_url"
+    else
+      ""
+    end
+  end
+
+  def handle_event("add_to_spotify", _, socket) do
+    sa = socket.assigns
+
+    playlist_id =
+      Dayhist.SpotifyAPI.CreatePlaylist.create_playlist_and_populate(
+        sa.user_info.nickname,
+        {
+          "#{sa.playlist_owner}'s #{sa.playlist.spotify_playlist_name}",
+          "This daylist is from #{sa.playlist.date |> Date.to_string()}. created with https://dayhi.st",
+          true
+        },
+        sa.playlist.contents
+      )
+
+    socket =
+      socket |> assign(:created_playlist_id, playlist_id)
+
+    {:noreply, socket}
   end
 
   def handle_params(%{"playlist" => playlist}, uri, socket) do
@@ -68,4 +100,13 @@ defmodule DayhistWeb.PlaylistLive do
 
   defp uuid_to_dashed_uuid(playlist_uuid),
     do: playlist_uuid |> String.replace(@uuid_regex, "\\1-\\2-\\3-\\4-\\5")
+
+  defp add_links(description) do
+    regex = ~r|<a href="spotify:playlist:(?<id>[^"]+)">(?<name>[^<]+)</a>|
+
+    # Replace all matches in the given html_string.
+    Regex.replace(regex, description, fn _full_match, id, name ->
+      "<a class= \"text-blue-500\" href=\"https://open.spotify.com/playlist/#{id}\">#{name}</a>"
+    end)
+  end
 end
