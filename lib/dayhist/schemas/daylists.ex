46acad56c90137cea03999b99c7ee41bbdd5ce3e
Owen Halliday <hallidayo@moravian.edu>
2024-08-10 12:45:04 -0400

    change Flop functions to be less redundant

--- lib/dayhist/schemas/daylists.ex
+++ lib/dayhist/schemas/daylists.ex
@@ -1,12 +1,11 @@
 defmodule Dayhist.Daylists do
   alias Dayhist.Schemas.Daylist
 
-  def list_daylists(params, _user_id) do
-    IO.inspect(params)
+  def list_daylists(params) do
     Flop.validate_and_run(Daylist, params, for: Daylist, repo: Dayhist.Repo)
   end
 
-  def get_daylists_by_user_id(user_id) do
-    Flop.validate_and_run(Daylist, %{user_id: user_id}, for: Daylist, repo: Dayhist.Repo)
+  def list_daylists(params, user_id) do
+    list_daylists(params |> Map.put(:user_id, user_id))
   end
 end
